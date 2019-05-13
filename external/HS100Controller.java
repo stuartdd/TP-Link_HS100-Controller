535270f10fb3bca1c6abd1189f0557c8d9386c97
Stuart <stuart.d.davies@btinternet.com>
2019-05-13 23:53:31 +0100

    Update HS100Controller.java

--- external/HS100Controller.java
+++ external/HS100Controller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Stuiart Davies (stuartdd)
+ * Copyright (C) 2019 Stuiart Davies (stuartdd)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
